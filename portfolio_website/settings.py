ac57a9449e3149b11fcc119debf6bb6da67292d4
Ravhan Shafi <80163239+sayimshah@users.noreply.github.com>
2023-03-02 11:15:53 +0530

    new

--- portfolio_website/settings.py
+++ portfolio_website/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-46&f&0!o6vk6xm4hrun!gf0@i9tyq$@qib+6fwpfk!t#j-q6ar
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
